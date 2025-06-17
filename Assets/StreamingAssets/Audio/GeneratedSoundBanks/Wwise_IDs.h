b0c42376ae4354e4ccc5361ccab2ae864334f2b3
JuliaHummer <146169340+JuliaHummer@users.noreply.github.com>
2025-06-17 13:28:54 +0200

    Created new Scene SampleSceneAudio, implemented Music and Footsteps of player and partly Companion ☠️

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY__FOOTSTEPSLEVEL = 1787411852U;
+        static const AkUniqueID PLAY__NEVERGONNAGIVEYOUUP = 2656427947U;
+        static const AkUniqueID PLAY__SOFTSTEPS = 2578942166U;
+        static const AkUniqueID PLAY__WAKEMEUP = 2477745328U;
+        static const AkUniqueID PLAY_PA_MUSIC = 3408178795U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID LEVELENVIRONMENT = 1586582072U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
