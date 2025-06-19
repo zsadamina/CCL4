f8edb7f659cacac3343367f898f9ba57d36ef291
Koller Simon <simon-koller@gmx.at>
2025-06-19 15:35:11 +0200

    Refactor NavAgent Script

---
+++ Assets/Scripts/NPCs/Companion.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Companion : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    [SerializeField]
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+
+    }
+}
