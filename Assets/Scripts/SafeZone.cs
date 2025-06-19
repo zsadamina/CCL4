2bb94f7b0c571a2632e855c29d853787768db3f1
Kaja Kitzmüller <146169299+GegenteilVonGegenteil@users.noreply.github.com>
2025-06-19 14:35:47 +0200

    Logic to exit tutorial

--- Assets/Scripts/SafeZone.cs
+++ Assets/Scripts/SafeZone.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SafeZone : MonoBehaviour
 {
+    [SerializeField] Material inactiveMaterial;
+    [SerializeField] Material activeMaterial;
+    [SerializeField] GameObject safeZoneGround;
 
-    
+    private bool _isActive = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,15 @@ public class SafeZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _isActive = StateManager.allItemsCollected;
         
+        if (_isActive)
+        {
+            safeZoneGround.GetComponent<Renderer>().material = activeMaterial;
+        }
+        else
+        {
+            safeZoneGround.GetComponent<Renderer>().material = inactiveMaterial;
+        }
     }
 }
