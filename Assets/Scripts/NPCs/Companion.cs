96765fdf6b351c99dfc7de836518a4515de5d102
Koller Simon <simon-koller@gmx.at>
2025-06-19 16:03:56 +0200

    Navigation Check

--- Assets/Scripts/NPCs/Companion.cs
+++ Assets/Scripts/NPCs/Companion.cs
@@ -5,19 +5,30 @@ using UnityEngine.AI;
 
 public class Companion : MonoBehaviour
 {
-    private NavMeshAgent navMeshAgent;
+    public float stoppingDistanceThreshold = 0.5f; // Adjust based on agent's stopping distance
+    
+    private NavMeshAgent _navMeshAgent;
+    
     [SerializeField]
     private Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        _navMeshAgent.SetDestination(target.position);
 
+        if (_navMeshAgent.hasPath && _navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance + stoppingDistanceThreshold)
+        {
+            // Agent is moving towards a destination
+        }
+        else
+        {
+            // Agent is standing still or has reached its destination
+        }
     }
 }
