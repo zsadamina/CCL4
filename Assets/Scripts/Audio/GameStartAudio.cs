124e9745f58d8f8ef8eceb2354fb3c1fda4817c7
JuliaHummer <146169340+JuliaHummer@users.noreply.github.com>
2025-06-20 07:48:11 +0200

    erhrrshhtdf

--- Assets/Scripts/Audio/GameStartAudio.cs
+++ Assets/Scripts/Audio/GameStartAudio.cs
@@ -23,7 +23,7 @@ public class GameStartAudio : MonoBehaviour
 
     private IEnumerator PlayIntroSequence()
     {
-        yield return new WaitForSeconds(1.0f); 
+        yield return new WaitForSeconds(0.5f); 
         dropEvent.Post(dropSoundSource);
 
         yield return new WaitForSeconds(delay);
