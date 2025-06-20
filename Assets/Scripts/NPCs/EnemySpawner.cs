36c6e2f38a45bb76aa4130d9816317a359cbcd2b
Koller Simon <simon-koller@gmx.at>
2025-06-20 03:09:12 +0200

    Enemy Spawner

--- Assets/Scripts/NPCs/EnemySpawner.cs
+++ Assets/Scripts/NPCs/EnemySpawner.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private float _enemySpawnStart;
+
+    
+     [SerializeField] private float _enemySpawnInterval;
+
+    
+     [SerializeField] private GameObject _enemy;
+    
+    private RoomGenerator _roomGenerator;
+    
+    void Awake()
+    {
+        _roomGenerator =  RoomGenerator.Instance;
+    }
     void Start()
     {
-        
+        InvokeRepeating("SpawnEnemy", _enemySpawnStart * 60, _enemySpawnInterval * 60);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnEnemy()
     {
-        
+        Debug.Log("Spawner Invoked");
+       var existingEnemy =  GameObject.FindGameObjectWithTag(_enemy.tag);
+       if (existingEnemy)
+       {
+           return;
+       }
+       var spawnObject =  _roomGenerator.paths.OrderBy( x => Random.value).First();
+       var enemy = Instantiate(_enemy, spawnObject.transform.position, Quaternion.identity);
     }
+    
 }
