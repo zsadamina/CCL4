52a1f515c9aa79eb585b94bba7c3ae3e3ea43e1f
Kaja Kitzmüller <146169299+GegenteilVonGegenteil@users.noreply.github.com>
2025-06-23 15:46:36 +0200

    Packeged up

---
+++ Packages/ca.tim-cstj.export-project-to-zip/Editor/ExportProjectToZipSettings.cs
@@ -0,0 +1,34 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExportProjectToZip
+{
+    /// <summary>
+    /// Settings for the ExportProjectToZip package.
+    /// </summary>
+    [System.Serializable]
+    public class ExportProjectToZipSettings
+    {
+        const bool shouldIncludeBuildsDefault = false;
+        const bool shouldNameRootLevelFolderWithZipNameDefault = true;
+        static readonly List<string> foldersToExcludeDefault = new() { ".git", ".vs", ".vscode", "Build", "Builds", "Library", "Logs", "obj", "Obj", "UserSettings", "Temp"};
+        static readonly List<string> topLevelExtensionsToExcludeDefault = new() { ".gitignore", ".sln", ".csproj", ".zip"};
+        public bool shouldIncludeBuilds = shouldIncludeBuildsDefault;
+        public bool shouldNameRootLevelFolderWithZipName = shouldNameRootLevelFolderWithZipNameDefault;
+        public List<string> foldersToExclude = new(foldersToExcludeDefault);
+        public List<string> topLevelExtensionsToExclude = new(topLevelExtensionsToExcludeDefault);
+
+        /// <summary>
+        /// Restore the default settings.
+        /// </summary>
+        public void RestoreDefaults()
+        {
+            shouldIncludeBuilds = shouldIncludeBuildsDefault;
+            shouldNameRootLevelFolderWithZipName = shouldNameRootLevelFolderWithZipNameDefault;
+            foldersToExclude = new(foldersToExcludeDefault);
+            topLevelExtensionsToExclude = new(topLevelExtensionsToExcludeDefault);
+        }
+    }
+}
+#endif
